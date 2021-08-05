0db2e7d7de2ab148c19075882bc515ee2be1f974
Tanvi Bhakta <tanvibhakta@gmail.com>
2021-08-05 10:45:11 +0530

    extremely basic file upload thingy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [selectedFile, setSelectedFile] = useState(null);
+  const onFileChange = event => setSelectedFile(event.target.files[0])
+  const urlAPI = "localhost:3001"
+  // On file upload (click the upload button)
+  const onFileUpload = () => {
+    const formData = new FormData();
+    // Update the formData object
+    formData.append(
+        "myFile",
+        selectedFile,
+        selectedFile.name
+    );
+    // Details of the uploaded file
+    console.log(selectedFile);
+    // Request made to the backend api
+    // Send formData object
+    axios.post(urlAPI, formData);
+  };
+
+  // File content to be displayed after file upload is complete
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+          <div>
+            <h2>File Details:</h2>
+            <p>File Name: {selectedFile.name}</p>
+            <p>File Type: {selectedFile.type}</p>
+            <p>
+              Last Modified:{" "}
+              {selectedFile.lastModifiedDate.toDateString()}
+            </p>
+          </div>
+      );
+    } else {
+      return (
+          <div>
+            <br />
+            <h4>Choose a file before Pressing the Upload button</h4>
+          </div>
+      );
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h3>
+            File Upload using React!
+          </h3>
+          <div>
+            <input type="file" onChange={onFileChange} />
+            <button onClick={onFileUpload}>
+              Upload
+            </button>
+          </div>
+          {fileData()}
+        </div>
       </header>
     </div>
   );
